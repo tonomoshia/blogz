7b43ec4f3e8557078c6beeaca996e1fbc2e29a6a
Tonomoshia <tonomoshia@gmail.com>
2019-10-10 20:52:15 -0400

    fixed indenting

--- main.py
+++ main.py
@@ -53,9 +53,9 @@ def login():
             if not user:
                 flash('Username does not exist', 'error')
                 return render_template('login.html')
-        elif password != user.password:
-            flash('Password is incorrect.', 'error')
-            return redirect('login')
+            elif password != user.password:
+                flash('Password is incorrect.', 'error')
+                return redirect('login')
 
     return render_template('login.html')
 
